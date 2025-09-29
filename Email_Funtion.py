7f364b4b440334415328548a4ad6044a0023647e
Edzel Manfoste <edzelmanfoste14@gmail.com>
2025-09-29 16:41:31 +0800

    Confirmation page/preview image

--- Email_Funtion.py
+++ Email_Funtion.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = "SnapBits@gmail.com"        
-app.config['MAIL_PASSWORD'] = "gwmgamelyazrrfco"
+app.config['MAIL_USERNAME'] = "xxxxxxxxx@gmail.com"        
+app.config['MAIL_PASSWORD'] = "xxxxxxxxxxxx"
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
